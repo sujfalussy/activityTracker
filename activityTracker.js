08fb4efdf7af5631d57298c380691cc60e665741
Stephen Ujfalussy <sujfalussy@zendesk.com>
2024-07-06 00:05:54 +0100

    Update activityTracker.js

--- activityTracker.js
+++ activityTracker.js
@@ -1,5 +1,6 @@
 (function() {
     // Check if the current page is the specified URL
+    console.log('Check if the current page is the specified URL');
     var targetPageUrl = "https://z3n-finservhelp.zendesk.com/hc/en-gb/categories/12057576841629-Opening-an-account";
     if (window.location.href === targetPageUrl) {
         // Make an API request to get the Zendesk user ID based on the logged-in email address
